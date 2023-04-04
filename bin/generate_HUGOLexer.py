608bc945af65fa7cdfc8a944be0e68da6b28d256
jeffquinn-msk <quinnj2@mskcc.org>
2023-04-04 12:44:02 -0400

    run black initially

--- bin/generate_HUGOLexer.py
+++ bin/generate_HUGOLexer.py
@@ -2,11 +2,13 @@ import json
 import requests
 import sys
 
+
 def get_latest_hugo_symbol_list():
     res = requests.get(
-        "https://ftp.ebi.ac.uk/pub/databases/genenames/hgnc/json/locus_types/gene_with_protein_product.json")
+        "https://ftp.ebi.ac.uk/pub/databases/genenames/hgnc/json/locus_types/gene_with_protein_product.json"
+    )
 
-    return [x['symbol'] for x in res.json()['response']['docs']]
+    return [x["symbol"] for x in res.json()["response"]["docs"]]
 
 
 def generate_lexer(output_file):
@@ -14,7 +16,9 @@ def generate_lexer(output_file):
 
     with open(output_file, "w") as f:
         f.write("lexer grammar HUGOLexer;\n\n")
-        f.write("GENE: " + " | ".join(f"'{gene_name}'" for gene_name in gene_names) + ";\n")
+        f.write(
+            "GENE: " + " | ".join(f"'{gene_name}'" for gene_name in gene_names) + ";\n"
+        )
 
 
 if __name__ == "__main__":
