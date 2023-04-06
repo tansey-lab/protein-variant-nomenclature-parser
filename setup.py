32e4d2d43d1f3d77b8b8e6a275f2b67f65b8c14e
jeffquinn-msk <quinnj2@mskcc.org>
2023-04-06 14:43:51 -0400

    done

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="protein-variant-nomenclature-parser",
-    version="0.3.0",
+    version="0.4.0",
     packages=find_packages(),
     url="https://github.com/tansey-lab/protein-variant-nomenclature-parser",
     author="Jeff Quinn",
